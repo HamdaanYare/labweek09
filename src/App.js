87c13633b134ffda6597c5cdc53c277f949e7e85
HamdaanYare <113747000+HamdaanYare@users.noreply.github.com>
2022-11-06 23:18:12 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './Student';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+            <h2>React JS Programming Week09 Lab exercise</h2>
+            <Student/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
